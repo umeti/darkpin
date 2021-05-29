6e0c21ed1e603d91e3098ea18c66934a830cb15b
umeti <yumeeti@gmail.com>
2021-05-29 16:57:32 +0800

    完善write页和测试API

---
+++ pages/api/write.dev.js
@@ -0,0 +1,14 @@
+import fs from "fs"
+
+export default (req,res) => {
+  console.log("calling /write.dev")
+  console.log(req.body)
+  let param = JSON.parse(req.body)
+  fs.writeFile("db/"+param.name, param.data, "utf8",(err)=>{
+    if (err){
+      res.status(500).json({message:err})
+      return
+    }
+    res.status(200).json({message:"OK"})
+  })
+}
--- pages/write.js
+++ pages/write.js
@@ -3,14 +3,62 @@ import A from "comp/a";
 import IconButton from "@material-ui/core/IconButton"
 import PublishRoundedIcon from '@material-ui/icons/PublishRounded';
 
-let tools = (<>
-  <IconButton color="inherit" edge="publish" aria-label="publish">
-    <PublishRoundedIcon />
-  </IconButton>
-</>);
+import TextField from "@material-ui/core/TextField"
+import InputBase from '@material-ui/core/InputBase';
+import { Divider, Paper } from "@material-ui/core";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
-export default function Write(props){
+
+
+export default function Write(props) {
+  let router = useRouter()
+  let [content, setContent] = useState("")
+  let tools = (<>
+    <IconButton color="inherit" 
+    onClick={()=>{send({
+      data:{
+        create_time: new Date().getTime(),
+        content
+      },
+      name: "_"+new Date().getTime()
+    },()=>{
+      router.replace("/")
+    },(err)=>{
+      alert(err)
+    })}}
+    disabled={content == ""}
+    edge="publish" 
+    aria-label="publish">
+      <PublishRoundedIcon />
+    </IconButton>
+  </>);
+
   return (<App title="Write" tools={tools} back>
-    
+
+    <InputBase
+      placeholder="hello world"
+      value={content}
+      onChange={(e) => setContent(e.target.value)}
+      multiline
+      fullWidth
+      autoFocus
+      rows={10}
+      rowsMax={1000}
+    />
+
+    <Divider />
+
   </App>)
+}
+
+function send(param, handleOk, handleError){
+  param.data = JSON.stringify(param.data)
+  fetch("/api/write.dev",{
+    method: "PUT",
+    body: JSON.stringify(param),
+  })
+  .then(res => res.json())
+  .then(data => handleOk(data))
+  .catch(err => handleError(err))
 }
